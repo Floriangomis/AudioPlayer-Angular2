4d40cdff130a94ea94d7079b29aad3c9e5508647
Floriangomis <florian.gomis@viacesi.fr>
2018-03-27 20:08:20 +0100

    :sparkle: Update Mocked Songs list

--- src/service/playlist.service.ts
+++ src/service/playlist.service.ts
@@ -13,6 +13,36 @@ export class PlaylistService {
             artist: 'Moussa'
         },
         {
+            title: 'Glue',
+            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DA7ZxRs45tTg&quality=128k',
+            cover: 'https://ninjatune.net/images/releases/aura-main.jpg',
+            artist: 'Bicep'
+        },
+        {
+            title: 'Blessed',
+            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D6jXOOBklBAw&quality=128k',
+            cover: 'https://cdn.pitchfork.com/longform/603/Smerz2.jpg',
+            artist: 'Smerz'
+        },
+        {
+            title: 'TTKTV',
+            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DUjD221A8RjQ&quality=128k',
+            cover: 'https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F48180d50daa0299ecab0c2993e418019.1000x1000x1.jpg',
+            artist: 'Injury Reserve'
+        },
+        {
+            title: 'The visitors',
+            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DjN38QWHuMKo&quality=128k',
+            cover: 'https://i1.sndcdn.com/artworks-000290161200-ui3gdi-t500x500.jpg',
+            artist: 'Mad Zac'
+        },
+        {
+            title: 'Leitmotiv',
+            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D0VMoLqPqK6s&quality=128k',
+            cover: 'https://f4.bcbits.com/img/0010107823_10.jpg',
+            artist: 'Dauwd'
+        },
+        {
             title: 'Oh Shit !',
             link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1-Mjw96lYgU&quality=128k',
             cover: 'https://images.genius.com/8d153823e72b48bc9b05be97f7d27141.1000x1000x1.jpg',
@@ -22,12 +52,6 @@ export class PlaylistService {
             title: 'All this money',
             link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DjcZ1Te9-1go&quality=128k',
             cover: 'https://images.genius.com/8d153823e72b48bc9b05be97f7d27141.1000x1000x1.jpg',
-            artist: 'Injury Reserve'
-        },
-        {
-            title: 'TTKTV',
-            link: 'https://www.1010diy.com/mp3?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DUjD221A8RjQ&quality=128k',
-            cover: 'https://t2.genius.com/unsafe/220x0/https%3A%2F%2Fimages.genius.com%2F48180d50daa0299ecab0c2993e418019.1000x1000x1.jpg',
             artist: 'Injury Reserve'
         }
     ];
